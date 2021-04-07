dc658cd665c11dbbf0449b6e000b71b7b08d513d
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-07 12:05:30 +0000

    Setup JPA relationships

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,9 @@
 package guru.springframework.spring5webapp.domain;
 
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -14,6 +17,8 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
     @ManyToMany
     @JoinTable
             (
@@ -21,15 +26,16 @@ public class Book {
                     joinColumns = @JoinColumn(name = "book_id"),
                     inverseJoinColumns = @JoinColumn(name = "author_id")
             )
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
+
 
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
+
     }
 
     public Long getId() {
@@ -62,6 +68,14 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,11 +1,10 @@
 package guru.springframework.spring5webapp.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 //Annotate publisher as Entity
 @Entity
@@ -22,24 +21,21 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
 
     public Publisher() {
     }
 
-    public Publisher(Long id, String name, String addressLine1, String city, String state, String zip) {
-        this.id = id;
-        this.name = name;
-        this.addressLine1 = addressLine1;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-    }
+
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
+
     public Long getId() {
         return id;
     }
@@ -82,6 +78,14 @@ public class Publisher {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
