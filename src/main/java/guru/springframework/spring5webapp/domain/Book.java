f47c8d98668dbb087fd1dd701bd5e174120d12df
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-04-07 17:31:19 +0000

    remove authors from to string to prevent infinite loop

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -84,7 +84,7 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
+
                 '}';
     }
 
